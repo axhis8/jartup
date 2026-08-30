f3c6f95bfdd9ec8cb46a80b89e7bc78cc9f90de1
axhis8 <161737873+axhis8@users.noreply.github.com>
2026-08-30 11:05:33 +0200

    add path variables

--- jartup.py
+++ jartup.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 
 def main():
@@ -14,8 +15,25 @@ def main():
     )
 
     args = parser.parse_args()
+    group_items: list[str] = args.group.split(".")
 
-    print(args)
+    # DIRS
+    root = Path(args.name)
+    main_dir = (
+        root / "src" / "main" / "java" / group_items[0] / group_items[1] / args.name
+    )
+    test_dir = (
+        root / "src" / "test" / "java" / group_items[0] / group_items[1] / args.name
+    )
+    resources_dir = root / "src" / "main" / "resources"
+
+    # FILES
+    main_java_file = main_dir / "Main.java"
+    gitignore_file = root / ".gitignore"
+    dockerignore_file = root / ".dockerignore"
+    dockerfile_file = root / "Dockerfile"
+    docker_compose_file = root / "docker-compose.yml"
+    pom_file = root / "pom.xml"
 
 
 if __name__ == "__main__":
