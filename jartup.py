8bdd1d63c119cb8e73bf0c622d67e6b483281fc1
axhis8 <161737873+axhis8@users.noreply.github.com>
2026-08-30 15:30:40 +0200

    handle errors, add rollback & maven flag

--- jartup.py
+++ jartup.py
@@ -1,27 +1,67 @@
 import subprocess
 import argparse
 import logging
+import shutil
 import sys
-from os.path import exists
 from pathlib import Path
 
+CACHED_DIR_SUFFIX = "_cached"
 
-def setup_logger():
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-    return logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+logger = logging.getLogger(__name__)
+
+
+def cache_old_dir(root):
+    logger.info("Backing up existing project...")
+    shutil.move(root, get_cached_path(root))
+
+
+def get_cached_path(root: Path) -> Path:
+    return Path(str(root) + CACHED_DIR_SUFFIX)
+
+
+def validate_template(template_path: Path, filename: str) -> Path:
+    file = template_path / filename
+    if not file.exists():
+        raise FileNotFoundError(
+            f"Required template file {filename} is missing in {template_path}!"
+        )
+
+    return file
+
+
+def rollback(error_msg: str, root: Path, old_dir_exists: bool):
+    logger.error(error_msg)
+
+    if root.exists():
+        logger.info(f"Cleaning up created directory '{root}'...")
+        shutil.rmtree(root)
+
+    if old_dir_exists:
+        logger.info("Restoring overwritten directory...")
+        shutil.move(get_cached_path(root), root)
+
+    sys.exit(1)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    logger = setup_logger()
 
+    # ARGS
     parser.add_argument("group", type=str, help="group name (example: 'com.name')")
     parser.add_argument("name", type=str, help="projekt name")
+    parser.add_argument(
+        "-m", "--maven", default=False, action="store_true", help="set up maven build"
+    )
     parser.add_argument(
         "-g", "--git", default=False, action="store_true", help="initialize git"
     )
     parser.add_argument(
-        "-d", "--docker", default=False, action="store_true", help="initialize docker"
+        "-d",
+        "--docker",
+        default=False,
+        action="store_true",
+        help="initialize docker (also sets up maven build)",
     )
     parser.add_argument(
         "--force",
@@ -32,57 +72,100 @@ def main():
 
     args = parser.parse_args()
     group_items: list[str] = args.group.split(".")
-    if len(group_items) != 2:
+
+    if len(group_items) < 2:
         logger.error("Invalid group arguments!")
         sys.exit(1)
 
+    if args.docker:
+        args.maven = True
+
     # DIRS
     root = Path(args.name)
-    if root.exists() and not args.force:
-        logger.error(f"Project '{args.name}' already exists!")
-        sys.exit(1)
 
-    main_dir = (
-        root / "src" / "main" / "java" / group_items[0] / group_items[1] / args.name
-    )
-    test_dir = (
-        root / "src" / "test" / "java" / group_items[0] / group_items[1] / args.name
-    )
-    resources_dir = root / "src" / "main" / "resources"
+    old_dir_cached = False
+    if root.exists() and not args.force:
+        rollback(f"Project '{args.name}' already exists!", root, old_dir_cached)
+
+    elif root.exists() and args.force:
+        cache_old_dir(root)
+        old_dir_cached = True
+
+    package_path = Path(*group_items)
+    template_path = Path(__file__).parent / "templates"
+
+    mvn_path = root / ".mvn"
+    main_path = root / "src" / "main" / "java" / package_path
+    test_path = root / "src" / "test" / "java" / package_path
+    resources_path = root / "src" / "main" / "resources"
 
     # FILES
-    main_java_file = main_dir / "Main.java"
     gitignore_file = root / ".gitignore"
     dockerignore_file = root / ".dockerignore"
     dockerfile_file = root / "Dockerfile"
     docker_compose_file = root / "docker-compose.yml"
     pom_file = root / "pom.xml"
     readme_file = root / "README.md"
+    mvnw_file = root / "mvnw"
+    mvnw_cmd_file = root / "mvnw.cmd"
+    main_java_file = main_path / "Main.java"
 
     # CREATE
     logger.info("Creating Directories and Files...")
 
-    main_dir.mkdir(parents=True, exist_ok=True)
-    test_dir.mkdir(parents=True, exist_ok=True)
-    resources_dir.mkdir(parents=True, exist_ok=True)
+    try:
+        main_path.mkdir(parents=True, exist_ok=True)
+        test_path.mkdir(parents=True, exist_ok=True)
+        resources_path.mkdir(parents=True, exist_ok=True)
+        main_java_file.touch(exist_ok=True)
 
-    main_java_file.touch(exist_ok=True)
-    pom_file.touch(exist_ok=True)
+        if args.maven:
+            logger.info("Setting up Maven Build...")
 
-    if args.git:
-        logger.info("Initializing Git...")
-        subprocess.run(["git", "init"], cwd=root)
-        gitignore_file.touch(exist_ok=True)
-        readme_file.touch(exist_ok=True)
+            mvnw_template = validate_template(template_path, "mvnw")
+            mvnw_cmd_template = validate_template(template_path, "mvnw.cmd")
+            mvn_dir_template = validate_template(template_path, ".mvn")
 
-    if args.docker:
-        logger.info("Setting up Docker...")
-        dockerignore_file.touch(exist_ok=True)
-        dockerfile_file.touch(exist_ok=True)
-        docker_compose_file.touch(exist_ok=True)
+            shutil.copytree(mvn_dir_template, mvn_path, dirs_exist_ok=True)
+            shutil.copy2(mvnw_cmd_template, mvnw_cmd_file)
+            shutil.copy2(mvnw_template, mvnw_file)
+            mvnw_file.chmod(0o755)
+            pom_file.touch(exist_ok=True)
+
+        if args.git:
+            logger.info("Initializing Git...")
+
+            gitignore_template = validate_template(template_path, ".gitignore")
+
+            subprocess.run(["git", "init"], cwd=root)
+            shutil.copy2(gitignore_template, gitignore_file)
+            readme_file.touch(exist_ok=True)
+
+        if args.docker:
+            logger.info("Setting up Docker...")
+
+            dockerignore_template = validate_template(template_path, ".dockerignore")
+            docker_compose_template = validate_template(
+                template_path, "docker-compose.yml"
+            )
+
+            shutil.copy2(dockerignore_template, dockerignore_file)
+            shutil.copy2(docker_compose_template, docker_compose_file)
+            dockerfile_file.touch(exist_ok=True)
+    except FileNotFoundError as e:
+        rollback(f"Error during project creation: {e}", root, old_dir_cached)
 
     # WRITING IN FILES
     logger.info("Writing in Files...")
+
+    # CLEAN UP
+    if old_dir_cached:
+        cached_path = get_cached_path(root)
+        if cached_path.exists():
+            logging.info("Removing cached Directory...")
+            shutil.rmtree(cached_path)
+
+    logging.info(f"Successfully created projekt {args.name}!")
 
 
 if __name__ == "__main__":
